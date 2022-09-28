1cae4b1bf79c0fc5fe1c5a5cffa470de8eea2ff5
Stuof3l <zehuaw@andrew.cmu.edu>
2022-09-28 19:05:59 -0400

    update main

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -12,7 +12,8 @@ package edu.cmu.cs.cs214.rec04;
 
 // HINT: Take a look at the UML diagram to see what DelegationSortedIntList
 //       should implement.
-public class DelegationSortedIntList implements IntegerList{
+public class DelegationSortedIntList implements IntegerList {
+    // the number of attempted element insertions
     private final SortedIntList sortedIntList;
 
     private int totalAdded;
@@ -32,38 +33,57 @@ public class DelegationSortedIntList implements IntegerList{
         this.totalAdded = 0;
         this.sortedIntList = new SortedIntList();
     }
-    
+
+    /**
+     * Add the specified int to the list and increment the totalAdded
+     * @param num to be added
+     * @return true if added successfully
+     */
     public boolean add(int num) {
         totalAdded++;
         return this.sortedIntList.add(num);
     }
 
+    /**
+     * Add all the elements of the IntegerLIst to the list and increment the
+     * totalAdded
+     * @param list IntegerList containing elements to be added
+     * @return true if added success
+     */
     public boolean addAll(IntegerList list) {
-        totalAdded += list.size();
+        totalAdded += list.size(); //This works in delegation
         return this.sortedIntList.addAll(list);
     }
 
-    @Override
+
+    /**
+     * Return the integer at the specified position in the list
+     * @param index index of the return element
+     * @return the element at the specified index
+     */
     public int get(int index) {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.sortedIntList.get(index);
+    }
+
+    /**
+     * Remove all the elements that are contained in the specified list
+     *
+     * @param list IntegerList containing elements that are to be removed
+     * @return true if success
+     */
+    public boolean removeAll(IntegerList list) {
+        return this.sortedIntList.removeAll(list);
+    }
+
+    /**
+     * @return the number of elements in this list.
+     */
+    public int size() {
+        return this.sortedIntList.size();
     }
 
     @Override
     public boolean remove(int num) {
-        // TODO Auto-generated method stub
         return false;
-    }
-
-    @Override
-    public boolean removeAll(IntegerList list) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public int size() {
-        // TODO Auto-generated method stub
-        return 0;
     }
 }
